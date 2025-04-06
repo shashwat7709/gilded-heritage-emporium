f7a176983b7943a1a903246e010dbbd9b6027929
Shweta Irali <shashwat7709@gmail.com>
2025-04-06 15:01:50 +0530

    refactor: decrease tilt effect rotation factor to 5 degrees for subtler interaction

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -55,7 +55,7 @@ const GallerySection = () => {
         {/* Gallery Grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {filteredItems.map((item) => (
-            <Tilt key={item.id}>
+            <Tilt key={item.id} rotationFactor={5}>
               <motion.div
                 variants={itemVariants}
                 initial="hidden"
